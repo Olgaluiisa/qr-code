74616c403d426ffe2370bfd845d6631b63b7ccdd
Olga Luisa <olgaluiisa@hotmail.com>
2022-11-02 17:36:45 -0300

    hotfix: spacing adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import imageQrCode from "./assets/image/image-qr-code.png"
 export const App = () => {
   return (
-    <section className="w-screen h-screen pt-14 bg-slate-300">
+    <section className="w-screen h-screen pt-2 bg-slate-300">
      <div className="w-[25rem] h-[38rem] bg-slate-50 my-0 mx-auto rounded-3xl pt-5 ">
        <figure className="mb-8">
         <img className="max-w-[90%] rounded-2xl my-0 mx-auto" src={imageQrCode} alt="imagem qr-code" />
